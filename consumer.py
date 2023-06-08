0d922b9597826795a6a8f01a37d76a6c4b550903
Chameera Rupasinghe <chameerarupasinghe@gmail.com>
2023-06-08 19:31:35 +0530

    initial commit

---
+++ consumer.py
@@ -0,0 +1,25 @@
+import websocket
+
+def on_open(ws):
+    print("Connection opened")
+    # Send a message to the WebSocket
+    ws.send("Hello, Chameera!")
+
+def on_message(ws, message):
+    print("Received message:", message)
+
+def on_error(ws, error):
+    print("Error:", error)
+
+def on_close(ws):
+    print("Connection closed")
+
+# Create a WebSocket connection
+ws = websocket.WebSocketApp("wss://ws.postman-echo.com/raw/",
+                            on_open=on_open,
+                            on_message=on_message,
+                            on_error=on_error,
+                            on_close=on_close)
+
+# Start the WebSocket connection
+ws.run_forever()
